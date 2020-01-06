67977f2a539f057a14aadb8933b03666936ad6e3
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-06 15:19:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,13 +13,13 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
+    if self.valid? && self.status == "pending" && sender.balance > amount
+      receiver.balance += amount
+      sender.balance -= amount
+      self.status = "complete"
     else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 
